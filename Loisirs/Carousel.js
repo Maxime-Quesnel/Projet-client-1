8a6c9d72c6d9b9ffc623e1375e550a40bbdf50ec
Trillat <mhtrillat@gmail.com>
2020-10-08 15:35:10 +0200

    Ajout du carousel

---
+++ Loisirs/Carousel.js
@@ -0,0 +1,16 @@
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].getElementsByClassName.display = "flex"
+//     }
+
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     slides[slideIndex-1].getElementsByClassName.display = "card";
+//     setTimeout(showSlides, 8000);
+// }
+
