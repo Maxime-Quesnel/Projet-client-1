07a326649ec7fd46683c420820c1a2286e8c42d7
Gael-s <gael.souchet@gmail.com>
2020-10-08 15:40:21 +0200

    mis a jour de l ensemble formulaire.html

--- Formulaire/Formulaire.js
+++ Formulaire/Formulaire.js
@@ -1,9 +1,29 @@
-const submit = document.querySelector(submit);
+const button = document.getElementById('submit');
+button.addEventListener('click', validation);
 
-submit.addEventListener(
+function validation(event) {
+    let commit = document.getElementById('commit').value;
+    let nom = document.getElementById('name').value;
+    let prenom = document.getElementById('prenom').value;
 
-if ()
-const name = document.querySelector(name);
-else ()
-console.log(name)
-)
+    document.querySelector('#commentname3').textContent = `${nom} ${prenom}`;
+    document.querySelector('#commentarea3').textContent = commit;
+
+    let commentarea1 = document.getElementById('commentarea1');
+    let commentarea2 = document.getElementById('commentarea2');
+    let commentarea3 = document.getElementById('commentarea3');
+    
+        // if (commentarea1.textContent === "") {
+        //     commentarea2.textContent = commit;
+        // }
+        //  else if (commentarea2.textContent === "") {
+        //     commentarea3.textContent = commit;
+        // }
+        //  else (commentarea3.textContent === ""); { 
+        //     commentarea1.textContent = commit;
+        // }
+        
+    }
+
+
+
